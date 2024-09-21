4ff67665cdacfbce944408030b3820c5e081e79c
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-09-21 22:29:46 +0300

    implement sign up with email and password method

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,6 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:recipe_app/core/errors/failure.dart';
+import 'package:recipe_app/core/errors/firebase_auth_failure.dart';
+import 'package:recipe_app/core/utils/app_localization_keys.dart';
 import 'package:recipe_app/features/auth/data/repos/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
@@ -26,8 +29,19 @@ class AuthRepoImpl implements AuthRepo {
 
   @override
   Future<Either<Failure, UserCredential>> signUpWithEmailAndPassword(
-      {required String email, required String password}) {
-    // TODO: implement signUpWithEmailAndPassword
-    throw UnimplementedError();
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return right(userCredential);
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        return left(FirebaseAuthFailure.fromFirebaseAuthException(e));
+      } else {
+        return left(
+          FirebaseAuthFailure(AppLocalizationKeys.auth.unknownError.tr()),
+        );
+      }
+    }
   }
 }
