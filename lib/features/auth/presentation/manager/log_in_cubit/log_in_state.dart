210c972438c888147b9a5fc33351774c3831d7ef
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-09-22 01:29:32 +0300

    add sign up & log in & forget password states

--- lib/features/auth/presentation/manager/log_in_cubit/log_in_state.dart
+++ lib/features/auth/presentation/manager/log_in_cubit/log_in_state.dart
@@ -4,3 +4,13 @@ part of 'log_in_cubit.dart';
 sealed class LogInState {}
 
 final class LogInInitial extends LogInState {}
+
+final class LogInSuccess extends LogInState {}
+
+final class LogInLoading extends LogInState {}
+
+final class LogInFailure extends LogInState {
+  final String errorMessage;
+
+  LogInFailure({required this.errorMessage});
+}
