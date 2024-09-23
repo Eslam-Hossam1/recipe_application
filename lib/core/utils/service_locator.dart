5659cd08206a945457f52a3a174b3c8df2c3f2e2
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-09-24 01:50:52 +0300

    refactor and modify service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,18 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Utils
 import 'package:recipe_app/core/utils/shared_prefernce_helper.dart';
+
+// Repos
 import 'package:recipe_app/features/auth/data/repos/auth_repo_impl.dart';
+
+// View Models
 import 'package:recipe_app/features/on_boarding/presentation/view_model/onboarding_view_model.dart';
 import 'package:recipe_app/features/splash/presentation/view_model/splash_view_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
   // Initialize SharedPreferences asynchronously
   final sharedPreferences = await SharedPreferences.getInstance();
-  getIt.registerSingleton<SplashViewModel>(SplashViewModel());
+
+  // Register SharedPreferencesHelper singleton
   getIt.registerSingleton<SharedPreferencesHelper>(
-      SharedPreferencesHelper(sharedPreferences));
-  getIt.registerSingleton<OnboardingViewModel>(OnboardingViewModel());
-  getIt.registerSingleton<AuthRepoImpl>(AuthRepoImpl());
+    SharedPreferencesHelper(sharedPreferences),
+  );
+
+  // Register SplashViewModel and OnboardingViewModel lazily
+  getIt.registerLazySingleton<SplashViewModel>(() => SplashViewModel());
+  getIt.registerLazySingleton<OnboardingViewModel>(() => OnboardingViewModel());
+
+  // Register AuthRepoImpl lazily
+  getIt.registerLazySingleton<AuthRepoImpl>(() => AuthRepoImpl());
 }
